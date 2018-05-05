907622293452c65f9cf8f7065d8558dd0c2df2fa
oheydrew <drew@oheydrew.me>
2018-05-05 20:27:03 +1000

    Update schema: profile migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504034405) do
+ActiveRecord::Schema.define(version: 20180505091305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180504034405) do
     t.text "avatar_data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "website"
+    t.boolean "email_show", default: true
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
